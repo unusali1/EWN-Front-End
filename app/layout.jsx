fab9c11375f2583e9596df1fc8309e3506a43280
unusali1 <mdunusali1906@gmail.com>
2025-08-20 00:26:28 +0600

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header.jsx";
-import Footer from "../components/Footer.jsx";
+import Header from "@/components/Header.jsx";
+import Footer from "@/components/Footer.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
